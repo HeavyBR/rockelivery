aacbb9b0c58a7bc2235b6e0cfdeba854243a0bf9
Matheus Cumpian <matheus.cumpian@sensedia.com>
2021-09-17 23:04:36 -0300

    feat: adding user create endpoint

--- lib/rockelivery/user.ex
+++ lib/rockelivery/user.ex
@@ -7,6 +7,8 @@ defmodule Rockelivery.User do
 
   @required_params [:age, :address, :cep, :cpf, :email, :password, :name]
 
+  @derive {Jason.Encoder, only: [:id, :age, :cpf, :address, :email]}
+
   schema "users" do
     field :age, :integer
     field :address, :string
@@ -25,8 +27,9 @@ defmodule Rockelivery.User do
     |> cast(params, @required_params)
     |> validate_required(@required_params)
     |> validate_length(:password, min: 6)
-    |> validate_length(:cpf, is: 8)
-    |> validate_length(:age, greater_than_or_equal_to: 18)
+    |> validate_length(:cpf, is: 11)
+    |> validate_length(:cep, is: 8)
+    |> validate_number(:age, greater_than_or_equal_to: 18)
     |> validate_format(:email, ~r/@/)
     |> unique_constraint([:email])
     |> unique_constraint([:cpf])
@@ -34,7 +37,7 @@ defmodule Rockelivery.User do
   end
 
   defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
-    change(changeset, %{password_hash: Pbkdf2.add_hash(password)})
+    change(changeset, Pbkdf2.add_hash(password))
   end
 
   defp put_password_hash(changeset), do: changeset
